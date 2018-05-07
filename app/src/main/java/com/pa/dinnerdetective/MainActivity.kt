de2695947aaded278b922ee7c9191dd4c638ebcd
Gregory Dunbar <Greg@gregdunbar.com>
2018-05-07 19:30:33 -0400

    update list and layouts

--- app/src/main/java/com/pa/dinnerdetective/MainActivity.kt
+++ app/src/main/java/com/pa/dinnerdetective/MainActivity.kt
@@ -2,11 +2,30 @@ package com.pa.dinnerdetective
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Chinese", "Steakhouse", "Pizza", "Burgers", "Fast Food", "Diner", "Italian", "Seafood", "Barbecue", "Japanese", "Bar", "Cafe or Bistro")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        decideBtn.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodText.text = foodList[randomFood]
+        }
+
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodTxt.text.toString()
+            foodList.add(newFood)
+            addFoodTxt.text.clear()
+        }
+
+
     }
 }
